c42f994503e317948dae983008d054eb7ad76137
lianrui.chu <lianrui.chu@17zuoye.com>
2019-08-02 14:30:56 +0800

    test TRACKER-3

--- sentry_tracker.py
+++ sentry_tracker.py
@@ -1,4 +1,7 @@
 import sentry_sdk
-sentry_sdk.init("https://9073cd843bbe4513852ab2ecce615e1b@sentry.io/1519850")
+
+sentry_sdk.init(
+    "https://9073cd843bbe4513852ab2ecce615e1b@sentry.io/1519850", release="test@0.0.0"
+)
 # test
 division_by_zero = 8 / 0
